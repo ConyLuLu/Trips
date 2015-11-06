a2ed91c88ff0575f83b47c13abcae925febcb4b3
ConyLuLu <v830121v@gmail.com>
2015-11-06 13:40:58 +0800

    logon、sign up success
    成功連結登入、註冊畫面

---
+++ www/js/app.js
@@ -0,0 +1,53 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+//angular.module('myTrip', ['ionic','ng-mfb'])
+
+angular.module('todoApp', ['ionic','ng-mfb','todoApp.controllers','todoApp.services'])
+
+.run(function($ionicPlatform,$state) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+    $state.go('login');
+    //"YOUR APP ID", "JAVASCRIPT KEY"
+    Parse.initialize("iTwpohuMLeF5sMxPFK7iiWWoagcUnoNcRr5G7zjX", "aReF1HkdLjQtpEdZy3xvFVxuYiL8H5Fa9qmqCv95");
+
+  });
+}).config(function($stateProvider, $urlRouterProvider){
+        $stateProvider.state('todos',{
+           url:'/todos',
+           controller:'TodoListController',
+           templateUrl:'views/todos.html'
+        }).state('createTodo',{
+            url:'/todo/new',
+            controller:'TodoCreationController',
+            templateUrl:'views/create-todo.html'
+        }).state('editTodo',{
+            url:'/todo/edit/:id/:content',
+            controller:'TodoEditController',
+            templateUrl:'views/edit-todo.html'
+        }).state('login', {
+            url: '/',
+            templateUrl: 'views/login.html',
+            controller: 'LoginCtrl'
+        }).state('signup', {
+            url: '/signup',
+            templateUrl: 'views/sign-up.html',
+            controller: 'LoginCtrl'
+        }).state('signin', {
+            url: '/signin',
+            templateUrl: 'views/loginto.html',
+            controller: 'LoginCtrl'
+          });
+         
+          $urlRouterProvider.otherwise("/");
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,124 @@
+/**
+ * Created by Sandeep on 11/09/14.
+ */
+angular.module('todoApp.controllers',['ng-mfb']).controller('TodoListController',['$scope','Todo',function($scope,Todo){
+
+    Todo.getAll().success(function(data){
+        $scope.items=data.results;
+    });
+
+    $scope.onItemDelete=function(item){
+        Todo.delete(item.objectId);
+        $scope.items.splice($scope.items.indexOf(item),1);
+    }
+
+}]).controller('TodoCreationController',['$scope','Todo','$state',function($scope,Todo,$state){
+
+    $scope.todo={};
+
+    $scope.create=function(){
+        Todo.create({content:$scope.todo.content}).success(function(data){
+            $state.go('todos');
+        });
+    }
+
+
+}]).controller('TodoEditController',['$scope','Todo','$state','$stateParams',function($scope,Todo,$state,$stateParams){
+
+    $scope.todo={id:$stateParams.id,content:$stateParams.content};
+
+    $scope.edit=function(){
+        Todo.edit($scope.todo.id,{content:$scope.todo.content}).success(function(data){
+            $state.go('todos');
+        });
+    }
+
+}]).controller('LoginCtrl', function($scope, $state) {
+ 
+  $scope.data = {};
+ 
+  $scope.signupEmail = function(){
+ 
+  //Create a new user on Parse
+  var user = new Parse.User();
+  user.set("username", $scope.data.username);
+  user.set("password", $scope.data.password);
+  user.set("email", $scope.data.email);
+ 
+  // other fields can be set just like with Parse.Object
+  user.set("somethingelse", "like this!");
+ 
+  user.signUp(null, {
+    success: function(user) {
+      // Hooray! Let them use the app now.
+      alert("success!");
+    },
+    error: function(user, error) {
+      // Show the error message somewhere and let the user try again.
+      alert("Error: " + error.code + " " + error.message);
+    }
+  });
+ 
+};
+ 
+  $scope.loginEmail = function(){
+    Parse.User.logIn($scope.data.username, $scope.data.password, {
+      success: function(user) {
+        // Do stuff after successful login.
+        console.log(user);
+        alert("success!");
+        $state.go('todos');
+      },
+      error: function(user, error) {
+        // The login failed. Check error to see why.
+        alert("error!");
+      }
+    });
+  };
+ 
+}).controller('MyTripCtrl', function($scope, $ionicModal, $state, $ionicHistory) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title,
+      startDate: task.startDate,
+      endDate: task.endDate,
+      image: "travel.jpg",
+      comment: task.comment,
+      like: false
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+    task.startDate = "";
+    task.endDate = "";
+    task.comment = "";
+  }
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  }
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  }
+
+  $scope.like = function(task) {
+    if (task.like == true)
+      task.like = false;
+    else 
+      task.like = true;
+
+  }
+});
